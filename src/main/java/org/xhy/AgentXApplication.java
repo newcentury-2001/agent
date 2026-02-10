1aba88375afe99495b795ae5aa64f283b2195707
newcentury-2001 <570383051@qq.com>
2026-02-10 22:39:44 +0800

    c1

--- src/main/java/org/xhy/AgentXApplication.java
+++ src/main/java/org/xhy/AgentXApplication.java
@@ -3,11 +3,13 @@ package org.xhy;
 import org.dromara.x.file.storage.spring.EnableFileStorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableFileStorage
+@EnableAsync
 public class AgentXApplication {
 
     public static void main(String[] args) {
